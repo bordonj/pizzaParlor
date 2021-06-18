47f63cbba018df90bc40f35ec4ace4dac2722250
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-18 09:26:56 -0700

    add PizzaOrder.prototype.addPrice()

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//business logic for Pizzas --------
+// business logic for Pizzas --------
 function Pizzas () {
   this.pizzaOrder = {};
   this.currentId = 0;
@@ -12,4 +12,29 @@ Pizzas.prototype.addPizzaOrder = function(pizzaOrder) {
 Pizzas.prototype.assignId = function() {
   this.currentId++;
   return this.currentId;
+}
+
+// business logic of PizzaOrder
+function PizzaOrder(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+PizzaOrder.prototype.addPrice = function() {
+  if (this.size === 'x-large') {
+    this.price = 10;
+  } else if (this.size === 'large') {
+    this.price = 8;
+  } else if (this.size === 'medium') {
+    this.price = 6;
+  } else {
+    this.price = 4;
+  }
+  if (this.toppings.length > 3) {
+    this.price += 6;
+  } else if (this.toppings.length === 2) {
+    this.price += 4;
+  } else {
+    this.price += 2;
+  }
 }
