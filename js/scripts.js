f9880be5af4e8dddc18ba13a276726cb9d97998e
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-20 10:59:56 -0700

    clean scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-// business logic for Pizzas
-//may delete this logic if unable to add multiple pizzaas
+// business logic for Pizzas ----------
 function Pizzas () {
   this.pizzaOrder = {};
   this.currentId = 0;
@@ -11,28 +10,16 @@ Pizzas.prototype.addPizzaOrder = function(pizzaOrder) {
   this.pizzaOrder[pizzaOrder.id] = pizzaOrder;
 };
 
-// fixed the deleting all orders breakikng code with line 16-
-// the above doesn't work if you only delete order
 Pizzas.prototype.assignId = function() {
-  // if (this.pizzaOrder.hasOwnProperty(Number)) {
-  //   this.currentId = 0;
-  // }
   let pizzaOrderEntries = Object.entries(this.pizzaOrder);
   if (pizzaOrderEntries === undefined) {
     this.currentId = 0;
   }
-
-  // if (this.pizzaOrder[1] === undefined) {
-  //   this.currentId = 0;
-  // }
   this.currentId++;
   return this.currentId;
-}
+};
 
 Pizzas.prototype.deleteOrder = function (id) {
-  if (this.pizzaOrder[id] === undefined) {
-    return false;
-  }
   delete this.pizzaOrder[id];
   for (let i = this.currentId; i >= 1; i--) {
     if (i > id) {
@@ -42,14 +29,10 @@ Pizzas.prototype.deleteOrder = function (id) {
       pizzas.currentId = id-1;
     }
   }
-  // if (this.pizzaOrder.hasOwnProperty(id)) {
-  //   console.log('hasOwnProperty', 'true');
-  // }
-  
   return true;
 };
 
-// business logic of PizzaOrder
+// business logic for PizzaOrder ----------
 function PizzaOrder(size, toppings) {
   this.size = size;
   this.toppings = toppings;
@@ -70,24 +53,17 @@ PizzaOrder.prototype.addPrice = function() {
   if (this.toppings.length >= 1) {
     for (let topping of this.toppings) {
       this.price += 1.50;
-      // console.log('loop', topping);
     }
-    // this.price += 6;
-  // } else if (this.toppings.length === 2) {
-  //   this.price += 4;
-  // } else if (this.toppings.length === 1) {
-  //   this.price += 2;
   } else {
     this.price;
   }
-}
+};
 
 
-// UI logic
+// UI logic ----------
 let pizzas = new Pizzas();
 let newPizza;
 
-// if you delete all orders, it breaks the object
 function attachDeleteListeners() {
   $(".orderPreview").on("click", ".delete", function() { 
     $(this).remove();
@@ -101,7 +77,7 @@ function attachDeleteListeners() {
 $(document).ready(function() {
   attachDeleteListeners()
   $('#addOrder').on('click', function(e) {
-    // it seems like I need to add the prevenDefault or it refreshes even though this isn't a submit
+    // seems like the prevenDefault() is needed, or page refreshes even though this isn't a type=submit
     e.preventDefault();
     let toppings = [];
     const selectedSize = $('#selectSize').val();
@@ -113,66 +89,41 @@ $(document).ready(function() {
           toppings.push($(this).val());
         }
       })
+      // initiates new pizzaOrder
       newPizza = new PizzaOrder(selectedSize, toppings);
-      
+      // adds order to pizzas object
       pizzas.addPizzaOrder(newPizza);
+      // add price to pizzaOrder
       newPizza.addPrice();
-      if (pizzas.currentId === 1) {
-        console.log('toppings', toppings);
-        console.log('newPizza', newPizza);
-        // newPizza.addPrice();
-        console.log('newPizza price', newPizza.price)
-        console.log('newPizza toppings', newPizza.toppings)
-        console.log('newPizza size', newPizza.size)
-        $('.price').html(`$${newPizza.price.toFixed(2)}`);
-        toppings = newPizza.toppings;
-        let toppingsStr = toppings.join('<br>');
-        console.log('toppingsStr', toppingsStr);
-        let htmlstr = `
-        <p><b>Size</b>: ${newPizza.size}</p>
-        <p><b>Toppings</b>: cheese<br>${toppingsStr}</p>
-        <p><b>Price</b>: ${newPizza.price}</p>`
-        $('.orderPreview').html(htmlstr);
-        $('.orderPreview').show();
-      } else {
-        $('.orderPreview').html('');
-        // to refresh values after add to cart 
-        $('#selectSize').val('select');
-        $('input').each(function() {
-          console.log('this', $(this))
-          $(this).prop('checked', false);
-        })
 
-        let pizzasArr = Object.keys(pizzas);
-        console.log('pizzasArr', pizzasArr)
-        console.log('pizzas object', pizzas);
-            // below only works for adding 2 pizzas, but more than 3 it breaks
-        for (let i = 1; i <= pizzas.currentId; i++) {
-          // added line below for if delete order in middle
-          if (pizzas.pizzaOrder[i] === undefined) {
-            continue;
-          }
-          console.log('pizzaOrder price', pizzas.pizzaOrder[i].price)
-          let displaySize = pizzas.pizzaOrder[i].size;
-          let displayToppings = pizzas.pizzaOrder[i].toppings;
-          let displayPrice = pizzas.pizzaOrder[i].price;
-          console.log('displaySize', displaySize);
-          console.log('displayToppings', displayToppings);
-          // toppings = newPizza.toppings;
-          let toppingsStr = displayToppings.join('<br>');
-          // console.log('toppingsStr', toppingsStr);
-          let htmlstr = `<div class='${i}'>
-          <p>Order ${i} <button class='delete' id='${i}'>delete</button></p>
-          <p><b>Size</b>: ${displaySize}</p>
-          <p><b>Toppings</b>: cheese<br>${toppingsStr}</p>
-          <p><b>Price:</b> ${displayPrice}</p><br>
-          </div>`
-          $('.orderPreview').append(htmlstr);
-          $('.orderPreview').show();;
+      $('.orderPreview').html('');
+      // lines 101 to 104 below refresh values after added to cart 
+      $('#selectSize').val('select');
+      $('input').each(function() {
+        $(this).prop('checked', false);
+      })
+      for (let i = 1; i <= pizzas.currentId; i++) {
+        if (pizzas.pizzaOrder[i] === undefined) {
+          continue;
         }
-        //reset price to 0 so doesn't keep looping when you add more pizzas
+        let displaySize = pizzas.pizzaOrder[i].size;
+        let displayToppings = pizzas.pizzaOrder[i].toppings;
+        let displayPrice = pizzas.pizzaOrder[i].price;
+        let toppingsStr = displayToppings.join('<br>');
+        let htmlstr = `<div class='${i}'>
+        <p>Order ---- <button class='delete' id='${i}'>delete</button></p>
+        <p><b>Size</b>: ${displaySize}</p>
+        <p><b>Toppings</b>: <br>cheese<br>${toppingsStr}</p>
+        <p><b>Price:</b> ${displayPrice}</p><br>
+        </div>`
+
+        $('.orderPreview').append(htmlstr);
+        $('.orderPreview').show();;
+      
+        // reset price to 0 so doesn't keep looping when you add more pizzas
         pizzas.totalPrice = 0;
       }
+      // displays totalPrice on UI
       for (let i = pizzas.currentId; i >= 1; i--) {
         if (pizzas.pizzaOrder[i] === undefined) {
           continue;
@@ -180,28 +131,9 @@ $(document).ready(function() {
         pizzas.totalPrice += pizzas.pizzaOrder[i].price;
       }
       $('.price').html(`$${pizzas.totalPrice.toFixed(2)}`);
-      console.log('totalPrice', pizzas.totalPrice)
-
     }
-
-
-
   })
-  // may delete this logic below if unable to correctly add pizzas 
-  // was able to refresh values with code below
-  // $('#addPizza').click(function(e) {
-  //   e.preventDefault();
-  //   $('#selectSize').val('select');
-  //   $('input').each(function() {
-  //     console.log('this', $(this))
-  //     $(this).prop('checked', false);
-  //   })
 
-  //   pizzas.addPizzaOrder(newPizza);
-  //   let pizzasArr = Object.keys(pizzas);
-  //   console.log('pizzasArr', pizzasArr)
-  //   console.log('pizzas object', pizzas);
-  // })
   $('#pizza').submit(function(e) {
     e.preventDefault();
     const selectedSize = $('#selectSize').val();
